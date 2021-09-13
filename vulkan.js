a4a9341705f9539dc51a0dab047884b68bd30ab9
Divyansh Dheer <divyanshdheer42@gmail.com>
2021-09-14 02:14:15 +0530

    Feat: Vulkan Translator
    Created a translator that fetches a translation API using fetch, and translates text into vulkan language.

---
+++ vulkan.js
@@ -0,0 +1,42 @@
+
+//NOTE: THE JS FILE IS LINKED AT THE END OF BODY TO PREVENT IT FROM RUNNING AT THE BEGINNING.
+
+//THIS API HAS A LIMIT OF 5 PER HOUR !!!!
+
+//url for the api, we've to fetch the api using the link
+
+//var url="https://api.funtranslations.com/translate/vulcan.json" 
+var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+//the format has to be .json?text=randon_text
+
+//text we want to translate, first we have to get it from HTML document using DOM principles
+var text= document.querySelector("#txt-one");
+var button=document.querySelector(".btn-style");
+
+//function for url
+function urlGenerator(inputText)
+{
+    
+    return url+"?text="+inputText;
+}
+//output function
+function translatedText(out)
+{
+    var outText= document.querySelector("#txt-two");
+    outText.innerText =out;
+}
+
+//function that performs as per click
+function clickHandler()
+{
+var inputText=text.value;
+console.log(inputText)
+//how fetch works[IMP !!]
+fetch(urlGenerator(inputText))
+.then(Response=>Response.json())
+.then(json=>translatedText(json.contents.translated));
+}
+
+//now for click event 
+//notes: there are no braces after clickHandler
+button.addEventListener("click", clickHandler);
